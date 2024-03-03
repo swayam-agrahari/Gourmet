4caa8356c3f5e1644ab5be6b65217a6d12e15cdc
Swayam Agrahari <swayamagrahari025@gmail.com>
2024-03-04 02:21:52 +0530

    added sub category and product items

---
+++ gourmet/src/app/pages/dashboard/[name]/page.tsx
@@ -0,0 +1,18 @@
+import Subcategory from '@/app/components/category/subcategory';
+import React from 'react'
+
+export default function Categories({params}:{
+    params:{
+        id:String;
+        name:String;
+      image:String;
+
+    }
+}) {
+  return (
+    <div>
+    <Subcategory id={params.id} name={params.name} image={params.image}/>
+      {params.id}
+    </div>
+  )
+}
