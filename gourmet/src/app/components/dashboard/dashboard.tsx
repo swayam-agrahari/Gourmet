34feee8a3f384029e713df5e30c4cc5d0ea9a7de
Swayam Agrahari <swayamagrahari025@gmail.com>
2024-03-03 23:59:35 +0530

    minor changes

--- gourmet/src/app/components/dashboard/dashboard.tsx
+++ gourmet/src/app/components/dashboard/dashboard.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Navbar from '../navbar'
 import { InfiniteMovingCardsDemo } from '@/app/pages/card/test-page'
 
-import Category from '../ui/category'
+import Category from '../category/category'
 
 import { CarouselSpacing } from '@/app/pages/favourite/testpage'
 
