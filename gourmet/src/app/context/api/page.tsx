aa0f12925c5d343643f7e5a15e1f9d2514243ecd
Swayam Agrahari <swayamagrahari025@gmail.com>
2024-03-06 20:23:53 +0530

    changed the file structure

---
+++ gourmet/src/app/context/api/page.tsx
@@ -0,0 +1,10 @@
+import Products from '@/app/components/category/products'
+import React from 'react'
+
+const page = () => {
+  return (
+    <div><Products/>()</div>
+  )
+}
+
+export default page
